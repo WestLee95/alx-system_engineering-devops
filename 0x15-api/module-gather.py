fa3df850136281e60e9bea3e88ffbd9a13db5364
West <eugenewestley95@gmail.com>
2024-07-29 06:11:40 -0700

    Module-doc

---
+++ 0x15-api/module-gather.py
@@ -0,0 +1,21 @@
+"""
+0-gather_data_from_an_API.py
+
+This script retrieves and displays the TODO list progress of an employee 
+based on their ID from a public API. It fetches user data to obtain the 
+employee's name and their associated tasks, displaying the number of 
+completed and total tasks along with the titles of the completed tasks.
+
+Usage:
+    python3 0-gather_data_from_an_API.py <employee_id>
+
+Parameters:
+    employee_id: An integer representing the employee's ID.
+
+Requirements:
+    - requests module
+
+Author: Eugene Westley
+Date: 29/7/2024
+"""
+
