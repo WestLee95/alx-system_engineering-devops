7e40f00e5e65936ef4b90a9bbdecbca96afce1f2
West <eugenewestley95@gmail.com>
2024-07-29 06:33:56 -0700

    Gather

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-
 import requests
 import sys
 
 
 def fetch_employee_todo_progress(employee_id):
     try:
-        # Retrieve user data to get the employee's name
+        # Fetch user data to get the employee's name
         user_response = requests.get(
             f'https://jsonplaceholder.typicode.com/users/{employee_id}')
         user_response.raise_for_status()  # Raise an exception for HTTP errors
@@ -14,23 +12,23 @@ def fetch_employee_todo_progress(employee_id):
 
         employee_name = user_data.get('name', 'Unknown')
 
-        # Gets todo list data for the employee
+        # Fetch todo list data for the employee
         todos_response = requests.get(
             f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
         todos_response.raise_for_status()
         todos_data = todos_response.json()
 
-        # Calculates the number of completed and total tasks
+        # Calculate the number of completed and total tasks
         total_tasks = len(todos_data)
         completed_tasks = [todo for todo in todos_data if todo['completed']]
         number_of_done_tasks = len(completed_tasks)
 
-        # Prints the employee's TODO list progress
+        # Display the employee's TODO list progress
         print(
             f"Employee {employee_name} is done with tasks(
                 {number_of_done_tasks} / {total_tasks}): ")
 
-        # This displays the title of each completed task
+        # Display the title of each completed task
         for task in completed_tasks:
             print(f"\t {task['title']}")
 
